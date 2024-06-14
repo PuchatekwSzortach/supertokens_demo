93faf15b943d0430b0bb29dccf675af136e60f59
Kuba Kolodziejczyk <kuba@conceptual.jp>
2024-06-14 13:29:18 +0530

    Replaced frontend and backend
    With projects generated from npx create-supertokens-app@latest --recipe=emailpassword

---
+++ frontend/src/config.ts
@@ -0,0 +1,16 @@
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+
+export const SuperTokensConfig = {
+  appInfo: {
+    appName: "SuperTokens Demo App",
+    apiDomain: "http://localhost:10003",
+    websiteDomain: "http://localhost:10002",
+  },
+  recipeList: [
+    EmailPassword.init({
+      useShadowDom: false,
+    }),
+    Session.init(),
+  ],
+};
