e0344708738b3358c29ab07422fac88dbd34fc2f
Kuba Kolodziejczyk <kuba@conceptual.jp>
2024-09-25 23:17:19 +0900

    Updated commit test and github actions
    - acceptance tests now wait on supertokens core to be ready to process requests
    - github actions now starts supporting containers before running tests

--- backend/tests/acceptance_tests/conftest.py
+++ backend/tests/acceptance_tests/conftest.py
@@ -2,9 +2,12 @@
 pytest conftest file with fixtures for acceptance tests
 """
 
+import time
+
 import box
 import pytest
 import requests
+import timeout_decorator
 
 import net.config
 import net.initialization
@@ -20,11 +23,55 @@ def test_config() -> box.Box:
     return net.utilities.get_config("/configurations/development_config.yaml")
 
 
+@pytest.fixture(scope="session")
+def session_scoped_test_config() -> box.Box:
+    """
+    Load the test configuration
+    """
+
+    return net.utilities.get_config("/configurations/development_config.yaml")
+
+
+@pytest.fixture(scope="session")
+def wait_on_supertokens_core(session_scoped_test_config):
+    """
+    Wait for supertokens core to be ready to accept requests
+    """
+
+    @timeout_decorator.timeout(30)
+    def probe():
+
+        is_ready = False
+
+        import icecream
+
+        while is_ready is False:
+
+            time.sleep(0.1)
+
+            icecream.ic(session_scoped_test_config.supertokens_core.docker_network.url)
+
+            try:
+                response = requests.get(
+                    session_scoped_test_config.supertokens_core.docker_network.url + "/hello",
+                    timeout=5
+                )
+
+                is_ready = response.status_code == 200
+
+            except (requests.exceptions.RequestException, requests.exceptions.ConnectionError):
+                pass
+
+    probe()
+
+
 @pytest.fixture(scope="function")
-def clean_supertokens_data(test_config):
+def clean_supertokens_data(wait_on_supertokens_core, test_config):
     """
     Clean supertokens data
     """
+
+    _ = wait_on_supertokens_core
 
     # First get all the users for public app
     response = requests.get(
